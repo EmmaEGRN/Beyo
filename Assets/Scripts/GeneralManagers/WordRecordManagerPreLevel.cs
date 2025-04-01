9058948c5ad5e296ed6f3035cad4dcb71a1e9e47
EmmaEGRN <echeverria.eam@gmail.com>
2025-04-01 17:42:29 -0600

    Bug fixed
    New words not added to dictionary bug fixed

--- Assets/Scripts/GeneralManagers/WordRecordManagerPreLevel.cs
+++ Assets/Scripts/GeneralManagers/WordRecordManagerPreLevel.cs
@@ -5,7 +5,7 @@ public class WordRecordManagerPreLevel : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private WordRecord wordRecord;
 
-    void Start()
+    void OnEnable()
     {
 
         LevelManager.onRightAnswer += AddNewWordToDictionary;
